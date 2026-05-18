fbbefdb2e542a2bdbb19655d4aa4ea58b16b77f0
Mayank Meghwal <125905142+itz-Mayank@users.noreply.github.com>
2026-05-18 22:10:55 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,46 @@
+// import axios from "axios";
+
+// const API_URL = "http://127.0.0.1:8000";
+
+// export const sendMessage = async (message: string) => {
+//   const response = await axios.post(`${API_URL}/chat`, {
+//     message,
+//   });
+
+//   return response.data;
+// };
+
+// export const continueTrip = async (
+//   destination: string,
+//   approvedSteps: string[]
+// ) => {
+
+//   const response = await axios.post(
+//     "http://127.0.0.1:8000/continue-trip",
+//     {
+//       destination,
+//       approved_steps: approvedSteps,
+//     }
+//   );
+
+//   return response.data;
+// };
+
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL;
 
-export const sendMessage = async (message: string) => {
-  const response = await axios.post(`${API_URL}/chat`, {
-    message,
-  });
+export const sendMessage = async (
+  message: string
+) => {
+
+  const response = await axios.post(
+    `${API_URL}/chat`,
+    {
+      message,
+    }
+  );
 
   return response.data;
 };
@@ -16,7 +51,7 @@ export const continueTrip = async (
 ) => {
 
   const response = await axios.post(
-    "http://127.0.0.1:8000/continue-trip",
+    `${API_URL}/continue-trip`,
     {
       destination,
       approved_steps: approvedSteps,
